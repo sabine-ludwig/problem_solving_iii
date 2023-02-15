b409c7e71fc66b34d15dddcd62a31f8ab5624022
Sabine Ludwig <sabinemichelle31@gmail.com>
2023-02-15 08:56:41 -0800

    added task instructions

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+Task 1: Happy Numbers
+A happy number is a number defined by the following process: starting with any positive integer, 
+replace the number by the sum of the squares of its digits, 
+and repeat the process until the number equals 1. An example of a happy number is 19
+(For more info: https://en.wikipedia.org/wiki/Happy_number)
+Write a method that determines if a number is happy or sad
+'''
+
+'''
+Task 2: Prime Numbers
+A prime number is a number that is only divisible by one and itself.
+Write a method that prints out all prime numbers between 1 and 100 
+'''
+
+'''
+Task 3: Fibonacci
+A series of numbers in which each number (Fibonacci number) is the sum of the two 
+preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+Write a method that does the Fibonacci sequence starting at 1
+HARDER VERSION: Write a method that does the Fibonacci sequence starting at a 
+number that a user inputs
+'''
+
